2902b29dd00af34e59d231b4e39e417b669e1cee
JongMok Lee <lijm1358@gmail.com>
2023-09-11 17:24:36 +0900

    add fisheye augmentations

--- datasets/augmentations.py
+++ datasets/augmentations.py
@@ -16,7 +16,9 @@ class BaseAugmentation():
             ]
         )
 
-    def __call__(self, image, mask):
+    def __call__(self, image, mask=None):
+        if mask is None:
+            return self.transform(image=image)
         return self.transform(image=image, mask=mask)
 
     def __repr__(self):
@@ -24,21 +26,20 @@ class BaseAugmentation():
     
 class FisheyeAugmentation():
     def __init__(self, p=0.5):
-        self.row_s = None
-        self.row_e = None
-        self.col_s = None
-        self.col_e = None
         self.transform = A.Compose(
             [
-                A.Resize(224, 224),
+                A.Resize(448, 448),
                 A.Normalize(),
                 ToTensorV2()
             ]
         )
         
-    def apply_fisheye(self, image):
+    def apply_fisheye(self, image, is_mask=False):
         # 이미지 크기 가져오기
         height, width = image.shape[:2]
+        
+        if is_mask:
+            image += 1
 
         # 카메라 매트릭스 생성
         focal_length = width / 8
@@ -53,29 +54,44 @@ class FisheyeAugmentation():
 
         # 왜곡 보정
         undistorted_image = cv2.undistort(image, camera_matrix, dist_coeffs)
+        
+        
+        if is_mask:
+            undistorted_image = np.where(undistorted_image==0, 13, undistorted_image)
+            undistorted_image -= 1
         return undistorted_image
     
-    def find_crop_axis(self, image):
-        width, height = image.shape[:2]
+    def find_crop_axis(self, image, mask):
+        row_s, row_e, col_s, col_e = None, None, None, None
+        height, width = image.shape[:2]
         row_blank = np.zeros((width, ))
         col_blank = np.zeros((1, height))
         
-        for i, row in enumerate(image):
-            if self.row_s is None and not np.equal(row_blank, row).all():
-                self.row_s = row
-            elif self.row_s is not None and np.equal(row_blank, row).all():
-                self.row_e = row
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
-    def crop_center(self, image):
-        return image[self.row_s:self.row_e, :]
+        for i, row in enumerate(gray_img):
+            if row_s is None and not np.equal(row_blank, row).all():
+                row_s = i
+            elif row_s is not None and np.equal(row_blank, row).all():
+                row_e = i
+                break
+        
+        for i, col in enumerate(gray_img.T):
+            if col_s is None and not np.equal(col_blank, col).all():
+                col_s = i
+            elif col_s is not None and np.equal(col_blank, col).all():
+                col_e = i
+                break
+            
+        return (image[row_s:row_e, col_s:col_e, :], mask[row_s:row_e, col_s:col_e])
+        
 
     def __call__(self, image, mask):
-        fisheye_image = self.apply_fisheye(image)
-        fisheye_mask = self.apply_fisheye(mask)
+        fisheye_image = self.apply_fisheye(image, False)
+        fisheye_mask = self.apply_fisheye(mask, True)
         
-        self.find_crop_axis(fisheye_image)
-        fisheye_image = self.crop_center(fisheye_image)
-        fisheye_mask = self.crop_center(fisheye_mask)
+        fisheye_image, fisheye_mask = self.find_crop_axis(fisheye_image, fisheye_mask)
+        
         return self.transform(image=fisheye_image, mask=fisheye_mask)
 
     def __repr__(self):
