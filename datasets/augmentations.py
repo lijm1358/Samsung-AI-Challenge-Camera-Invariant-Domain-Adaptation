df5b57771102d918a0ebd22ec8012f1555dc9473
JongMok Lee <lijm1358@gmail.com>
2023-09-11 21:25:06 +0900

    add args for fisheye augmentation

--- datasets/augmentations.py
+++ datasets/augmentations.py
@@ -3,14 +3,15 @@ from albumentations.pytorch import ToTensorV2
 
 import cv2
 import numpy as np
+import random
 
 class BaseAugmentation():
-    def __init__(self, p=0.5):
+    def __init__(self, resize=224, p=0.5):
         super(BaseAugmentation, self).__init__()
         
         self.transform = A.Compose(
             [   
-                A.Resize(224, 224),
+                A.Resize(resize, resize),
                 A.Normalize(),
                 ToTensorV2()
             ]
@@ -25,10 +26,15 @@ class BaseAugmentation():
         return self.__class__.__name__ + f"(p={self.p})"
     
 class FisheyeAugmentation():
-    def __init__(self, p=0.5):
+    def __init__(self, resize=224, focal_ratio=0.25, center_x_ratio=0.5, center_y_ratio=0.5, p=0.5):
+        self.focal = focal_ratio
+        self.center_x = center_x_ratio
+        self.center_y = center_y_ratio
+        self.p = p
+        
         self.transform = A.Compose(
             [
-                A.Resize(448, 448),
+                A.Resize(resize, resize),
                 A.Normalize(),
                 ToTensorV2()
             ]
@@ -42,11 +48,11 @@ class FisheyeAugmentation():
             image += 1
 
         # 카메라 매트릭스 생성
-        focal_length = width / 8
-        center_x = width / 2
-        center_y = height / 2
-        camera_matrix = np.array([[width/5, 0, center_x],
-                                [0, width/6, center_y],
+        focal_length = width * self.focal
+        center_x = width * self.center_x
+        center_y = height * self.center_y
+        camera_matrix = np.array([[focal_length, 0, center_x],
+                                [0, focal_length, center_y],
                                 [0, 0, 1]], dtype=np.float32)
 
         # 왜곡 계수 생성
@@ -87,10 +93,14 @@ class FisheyeAugmentation():
         
 
     def __call__(self, image, mask):
-        fisheye_image = self.apply_fisheye(image, False)
-        fisheye_mask = self.apply_fisheye(mask, True)
-        
-        fisheye_image, fisheye_mask = self.find_crop_axis(fisheye_image, fisheye_mask)
+        if random.random() < self.p:
+            fisheye_image = self.apply_fisheye(image, False)
+            fisheye_mask = self.apply_fisheye(mask, True)
+            
+            fisheye_image, fisheye_mask = self.find_crop_axis(fisheye_image, fisheye_mask)
+        else:
+            fisheye_image = image
+            fisheye_mask = mask
         
         return self.transform(image=fisheye_image, mask=fisheye_mask)
 
